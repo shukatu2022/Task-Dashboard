1d864b7f07dec0ad7c8ca0998ad040a8499d67df
Ina <enough981.shukatu2022@gmail.com>
2026-06-10 19:06:04 +0900

    import PrismaClient の削除

--- auth.ts
+++ auth.ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
 import GitHub from "next-auth/providers/github";
 import { prisma } from "@/lib/prisma";
 
